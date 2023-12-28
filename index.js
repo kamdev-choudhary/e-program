3db2c331ac62c5353db0b7349ffb840d1548bcf5
Kamdev choudhary <82713142+kamdev-choudhary@users.noreply.github.com>
2023-12-28 18:40:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
+app.set("views", path.join(__dirname, "views");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = 8080;
 const app = express();
-
-app.set("view engine", "ejs");
 
 app.listen(port, (req, res) =>{
     console.log(`server is liesting at port : ${port}`);
